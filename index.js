3d262eca15735bc173bd6fcb47d2c2d4574cb16a
omar-perez-cano-itlighten <omartian21@gmail.com>
2021-05-08 19:05:17 -0500

    Created post method, updated keys for cloudfoundry

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const port = 8080
+
+app.use(bodyParser.json());
+var cors = require('cors')
+
+app.use(cors())
+
+const {Translate} = require('@google-cloud/translate').v2;
+
+const TOKEN_ARG = 2;
+const tokenPath = process.argv[TOKEN_ARG];
+process.env.GOOGLE_APPLICATION_CREDENTIALS = tokenPath;
+
+const translate = new Translate();
+
+app.post('/translate', async (req, res) => {
+    let [translations] = await translate.translate(req.body.text, req.body.target);
+    res.send(translations);
+})
+
+app.get('/', (req, res) => {
+  res.send('PaaS para el servicio de Speech-to-text de la aplicación VoiceNoteIO')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
